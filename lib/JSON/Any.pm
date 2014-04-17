3f0fedcbfbf33ca78d7ed81bf2e73baccbbbf372
Karen Etheridge <ether@cpan.org>
2014-04-16 19:13:35 -0700

    [PodWeaver] already adds a NAME section

--- lib/JSON/Any.pm
+++ lib/JSON/Any.pm
@@ -295,10 +295,6 @@ sub import {
     croak "Couldn't find a encoder method." unless $encoder;
 }
 
-=head1 NAME 
-
-JSON::Any
-
 =head1 SYNOPSIS
 
 This module tries to provide a coherent API to bring together the various JSON
